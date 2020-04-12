e1c06050f12fd6cd550b28122bed7a6328c5b526
Shankar Khatri <yes143.shankar@gmail.com>
2020-04-12 00:39:37 -0500

    Add filter feature

--- spread.js
+++ spread.js
@@ -3,23 +3,38 @@ var app = new Vue({
   data: {
     products: {},
     sort_by: "spread",
+    checkedAssets: {
+      ALL: true,
+      USD: true,
+      USDC: true,
+      BTC: true,
+      EUR: true,
+      GBP: true,
+      DAI: true,
+      ETH: true,
+    },
   },
   computed: {
     product_ids: function () {
+      let filteredProducts = Object.keys(this.products).filter((pid) => {
+        let asset = pid.split("-")[1];
+        return this.checkedAssets[asset];
+      });
+
       if (this.sort_by === "spread") {
-        return Object.keys(this.products).sort(
+        return filteredProducts.sort(
           (a, b) => this.products[b].spread - this.products[a].spread
         );
       } else if (this.sort_by === "volatility") {
-        return Object.keys(this.products).sort(
+        return filteredProducts.sort(
           (a, b) => this.products[b].volatility - this.products[a].volatility
         );
       } else if (this.sort_by === "polarity") {
-        return Object.keys(this.products).sort(
+        return filteredProducts.sort(
           (a, b) => this.products[b].polarity - this.products[a].polarity
         );
       } else {
-        return Object.keys(this.products).sort();
+        return filteredProducts.sort();
       }
     },
   },
@@ -29,6 +44,13 @@ var app = new Vue({
     },
     poleSide: function (value) {
       return value >= 0 ? "green" : "red";
+    },
+    toggleCheckboxes: function () {
+      for (const key in this.checkedAssets) {
+        if (this.checkedAssets.hasOwnProperty(key)) {
+          this.checkedAssets[key] = this.checkedAssets.ALL;
+        }
+      }
     },
   },
 });
